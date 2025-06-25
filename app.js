e7dd117a764541d99785a11c12b947c9051bfea9
mkhmmd17 <maxmelhi95@gmail.com>
2025-06-24 20:42:37 -0400

    app commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('server running on localhost:3000');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the subscription Tracker API');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
+
+export default app;
